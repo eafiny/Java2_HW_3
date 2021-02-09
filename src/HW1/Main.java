625f974983273867c93a242292406be8e1214437
Evgenia <zhenja_a@bk.ru>
2021-02-09 14:36:02 +0300

    Готовые классы

--- src/HW1/Main.java
+++ src/HW1/Main.java
@@ -1,4 +1,28 @@
 package HW1;
 
+import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+        String[] words = new String[]{
+          "Java", "Javascript", "Python","Ruby", "Pascal", "C++", "Java", "C",
+          "C++", "Java", "Assembler", "DAX", "DAX", "Java", "Prolog"
+        };
+        Set<String> wordSet= new HashSet<>();
+        System.out.println("Список уникальных слов в массиве:");
+        for (String word:words) {
+            if (wordSet.add(word)) System.out.println(word);
+        }
+
+        Map<String, Integer> wordCount = new HashMap<>();
+        for (String s:wordSet) {
+            wordCount.put(s,0);
+        }
+        for (String s:words) {
+            wordCount.put(s, wordCount.get(s) + 1);
+        }
+        for (String key:words) {
+            System.out.println("Слово " + key + " встречается " + wordCount.get(key) + "раз");
+        }
+    }
 }
--- src/HW2/PhoneBook.java
+++ src/HW2/PhoneBook.java
@@ -1,4 +1,31 @@
 package HW2;
 
+import java.util.*;
+
 public class PhoneBook {
+    Map<String, List<String>> telSpravochnik = new HashMap<>();
+
+    public void add(String surname, String number){
+        List<String> addList = new ArrayList<>();
+        if (telSpravochnik.containsKey(surname)) {
+            addList = telSpravochnik.get(surname);
+        }
+            addList.add(number);
+            telSpravochnik.put(surname, addList);
+    }
+
+    public List<String> get (String surname){
+        return telSpravochnik.get(surname);
+    }
+
+    public static void main(String[] args) {
+        PhoneBook myBook = new PhoneBook();
+        myBook.add("Petrov", "+74956258963");
+        myBook.add("Ivanov", "+74999632541");
+        myBook.add("Orlova", "+79584123698");
+        myBook.add("Ivanov", "+79154861256");
+
+        System.out.println(myBook.get("Ivanov"));
+
+    }
 }
